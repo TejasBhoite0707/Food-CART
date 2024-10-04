f77fa6360738267cb4511911ee16e3e9fdcf7805
surajpatil630304@gmail.com <tejasbhoite3006@gmail.com>
2024-10-04 20:27:51 +0530

    aunthentication90%done

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector,useDispatch } from 'react-redux';
 import { setSearch } from './Slices/SearchSlice';
 import { useForm } from 'react-hook-form';
@@ -6,6 +6,7 @@ import toast from 'react-hot-toast';
 import {useLocation, useNavigate } from 'react-router-dom'
 import axios from 'axios';
 const Navbar =() => {
+ 
 let location=useLocation()
 let navigate=useNavigate()
 let from=location.state?.from.pathname || '/' 
@@ -38,6 +39,10 @@ const onSubmit= async(data)=>{
 
 
 const dispatch=useDispatch();
+const users=JSON.parse(localStorage.getItem('UsersLogedIn'))
+let [authenticate,setAunthenticate]=useState(users?true:false)
+console.log(authenticate);
+
     return (
         <nav className='flex flex-col lg:flex-row justify-between mx-6 py-3'>
 <div>
@@ -49,29 +54,35 @@ const dispatch=useDispatch();
     className='p-3 border border-gray-800 outline-none rounded-lg text-sm w-full lg:w-[25vw]'
   
    />
-    <button className="btn bg-red-500 text-white" onClick={()=>document.getElementById('my_modal_3').showModal()}>Order Now</button>
+    <button className="btn bg-red-500 text-white" onClick={()=>authenticate? setInterval(()=>{
+    localStorage.removeItem('UsersLogedIn')
+     
+    window.location.reload()
+    
+  },1000): document.getElementById('my_modal_3').showModal()}>{authenticate?"LOGOUT":"ORDER_NOw"}</button>
+    
 <dialog id="my_modal_3" className="modal">
   <div className="modal-box">
     <form method="dialog" action='' onSubmit={handleSubmit(onSubmit)}>
       {/* if there is a button in form, it will close the modal */}
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={()=>document.getElementById('my_modal_3').close()}>✕</button>
     
     <h3 className='text-xl '>Sign up</h3>
     <span>username</span><br/>
    <input type='text' name='username' placeholder='Enter Your Name' className='w-96 outline-none border-2 border-zinc-400 rounded-md px-3 py-1'
    {...register('username',{required:true})} 
    /><br/>
-    {errors.username && <span className='text-sm text-red-400'>This field is required</span>}
+    {errors.username && <span className='text-sm text-red-400'>This field is required</span>}<br/>
    <span>Email</span><br/>
    <input type='email' name='email' placeholder='Enter Your Email' className='w-96 outline-none border-2 border-zinc-400 rounded-md px-3 py-1'
    {...register('email',{required:true})} 
    /><br/>
-   {errors.email && <span className='text-sm text-red-400'>This field is required</span>}
+   {errors.email && <span className='text-sm text-red-400'>This field is required</span>}<br/>
    <span>password</span><br/>
    <input type='password' name='password' placeholder='Enter Your password' className='w-96 outline-none border-2 border-zinc-400 rounded-md px-3 py-1'
    {...register('password',{required:true})} 
    /><br/>
-   {errors.password && <span className='text-sm text-red-400'>This field is required</span>}
+   {errors.password && <span className='text-sm text-red-400'>This field is required</span>}<br/>
    <div className='flex justify-between items-center mt-2'>
    <input type='submit' value='Sign Up' className='px-3 py-1 bg-blue-500 rounded-md '/>
    <span>Already Login <a className='underline cursor-pointer text-blue-700' href='/login'>Login</a></span>
--- frontend/src/Components/login.jsx
+++ frontend/src/Components/login.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useForm} from 'react-hook-form'
+import { Link, useNavigate } from 'react-router-dom'
 import toast from 'react-hot-toast'
 import axios from 'axios'
 function Login () {
+  
+  
+  
+  let navigate=useNavigate();
   const{
     register,
     handleSubmit,
@@ -13,14 +18,17 @@ let userinfo={
   email:data.email,
   password:data.password
 };
+
 await axios.post('http://localhost:4002/user/login',userinfo).then((res)=>{
   console.log(res.data);
   if(res.data){
+    navigate('/')
     toast.success("Login Successfully")
+   
     document.getElementById('my_modal_3').close()
     setTimeout(()=>{
      // window.location.reload()
-      localStorage.setItem('users',JSON.stringify(res.data.user))
+      localStorage.setItem('UsersLogedIn',JSON.stringify(res.data.user))
     },1000)
   }
   
@@ -41,21 +49,21 @@ await axios.post('http://localhost:4002/user/login',userinfo).then((res)=>{
   <div className="modal-box">
     <form method="dialog" onSubmit={handleSubmit(onSubmit)}>
       {/* if there is a button in form, it will close the modal */}
-     <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+   <Link to='/'> <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
       onClick={()=>document.getElementById('my_modal_3').close()}
-      >✕</button>
+      >✕</button></Link> 
     
     <h3 className="font-bold text-lg mb-2">Login Here</h3>
     <span>Email</span><br/>
     <input type='email' name='email' placeholder='Enter Your Email' className='w-96 outline-none border-2 border-zinc-400 rounded-md px-3 py-1'
     {...register('email',{required:true})}
     /><br/>
-    {errors.email && <span className='text-sm text-red-400'>This field is required</span>}
+    {errors.email && <span className='text-sm text-red-400'>This field is required</span>}<br/>
    <span>password</span><br/>
    <input type='password' name='password' placeholder='Enter Your password' className='w-96 outline-none border-2 border-zinc-400 rounded-md px-3 py-1'
    {...register('password',{required:true})}
    /><br/>
-   {errors.password && <span className='text-sm text-red-400'>This field is required</span>}
+   {errors.password && <span className='text-sm text-red-400'>This field is required</span>}<br/>
     <input type='submit' value='Login' className='bg-blue-600 rounded-md px-3 py-1 mt-3'/>
     </form>
   </div>
