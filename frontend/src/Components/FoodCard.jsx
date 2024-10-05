7fcde9181c3b876783b968db1730a77f1b82f0ea
surajpatil630304@gmail.com <tejasbhoite3006@gmail.com>
2024-10-05 22:39:57 +0530

    State Managed

--- frontend/src/Components/FoodCard.jsx
+++ frontend/src/Components/FoodCard.jsx
@@ -1,13 +1,17 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { IoIosStar } from "react-icons/io";
 import { useDispatch } from 'react-redux';
 import { addToCart } from './Slices/CartSlice';
 import CartSlice from './Slices/CartSlice';
+import { useNavigate } from 'react-router';
 
 const FoodCard =({id,rating,img,name,price,desc,handleToast}) => {
 const dispatch=useDispatch();
-
+let users=JSON.parse(localStorage.getItem('UsersLogedIn'));
+const [authenticate,setAunthenticate]=useState(users?true:false);
+console.log(authenticate);
+let navigate=useNavigate();
     return (
        <div className='fond-bold bg-white w-[250px] p-4 flex-col rounded-lg gap-2'>
        
@@ -28,9 +32,15 @@ const dispatch=useDispatch();
 <IoIosStar className='text-yellow-400 mr-1'/>{rating}
         </span>
         <button className='p-1 text-white bg-green-400  hover:bg-green-700 rounded-lg text-sm'
-        onClick={()=>{
+        onClick={()=>{if(authenticate){
             dispatch(addToCart({id,name,price,rating,img,qty:1}));
-            handleToast(name);}}
+                handleToast(name);
+        }
+        else
+        (navigate('/login'))}}
+
+           
+                
         >Add to cart</button>
        </div>
 
